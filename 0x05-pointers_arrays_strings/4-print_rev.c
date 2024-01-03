e9aefdde9051bf45aa23f86224950bc2c6179e77
9603nix <mikemachage@gmail.com>
2024-01-03 16:04:58 +0300

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count - l ; i  >= 0 ; i--)
+	for (i = count - 1; i  >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
