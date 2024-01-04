05d268a4f2b29f4522ef6f981557711e9eeea05e
9603nix <mikemachage@gmail.com>
2024-01-05 02:23:29 +0300

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = 0;
 	int i;
 
-	while (dest[dest_len] != '\0' ; i++)
+	while (dest[dest_len] != '\0')
 		dest_len++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
