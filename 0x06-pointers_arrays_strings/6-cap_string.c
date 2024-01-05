e3d3e92361aaeaff61dfd4a67c718006d2700245
9603nix <mikemachage@gmail.com>
2024-01-05 08:33:43 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,29 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: the input string
- * Return: pointer to the modified string
+ * cap_string - capitalizes all words in a string
+ * @str: input string
+ * Return: modified string
  */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i, j;
+	int i;
 
-char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= ('a' - 'A');
 
-for (i == 0; s[i] != '\0'; i++)
-
-{
-
-if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-
-s[i] -= 32;
-
-for (j = 0; j < 13; j++)
-
-{
-if (s[i] == spe[j])
-{
-	if  (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-		s[i + 1] -= 32;
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+				(str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+				 str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+				 str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+				 str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+				 str[i - 1] == '}'))
+		{
+			str[i] -= ('a' - 'A');
+		}
+	}
+	return (str);
 }
-}
-}
-}
-
