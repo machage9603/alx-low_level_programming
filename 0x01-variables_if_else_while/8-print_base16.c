593cb0d4f56bace14edd53c3caa58464d10bc38c
9603nix <mikemachage@gmail.com>
2024-01-07 02:44:58 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char hex_digits[] = "0123456789abcdef";
+
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex_digits[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
