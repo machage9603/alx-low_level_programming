f8604820fefb8498bb59049fd51f469d31d9228a
9603nix <mikemachage@gmail.com>
2024-01-07 03:00:03 +0300

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	char hex_digits[] = "0123456789abcdef";
-	 int i;
 
-	 for (i = 0; i < 16; i++)
-	 {
-		 putchar(hex_digits[i]);
-	 }
-	 putchar('\n');
+	int i;
 
-	 return (0);
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex_digits[i]);
+	}
+	putchar('\n');
+
+	return (0);
 }
