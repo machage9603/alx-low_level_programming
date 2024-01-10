510ea85874d0bcf7af5d40bce3fd5d7f2907b625
9603nix <mikemachage@gmail.com>
2024-01-11 02:26:29 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s1 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(sa + 1, s2));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	return (0);
 }
