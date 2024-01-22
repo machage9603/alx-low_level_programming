00472bc4f740247d072f94a2b09393b2f94eacc0
9603nix <mikemachage@gmail.com>
2024-01-23 01:08:56 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines the list
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the doug
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
+
