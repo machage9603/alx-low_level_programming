bfe04a13f7b8bdad597753ac4c70b91a1faebeca
9603nix <mikemachage@gmail.com>
2024-01-25 03:24:08 +0300

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ *  * main - Entry point of the program.
+ *   * @argc: The number of command-line arguments.
+ *    * @argv: An array of strings containing the command-line arguments.
+ *     *
+ *      * Return: Always 0 (success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
