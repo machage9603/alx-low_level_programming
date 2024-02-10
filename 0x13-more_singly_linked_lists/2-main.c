eb528768e1f5206db5c60e81d51f3bdc5ffc5957
9603nix <mikemachage@gmail.com>
2024-02-10 17:01:31 +0300

    updated list

--- 0x13-more_singly_linked_lists/2-main.c
+++ 0x13-more_singly_linked_lists/2-main.c
@@ -20,7 +20,7 @@ int main(void)
     add_nodeint(&head, 4);
     add_nodeint(&head, 98);
     add_nodeint(&head, 402);
-    
+    add_nodeint(&head, 1024); 
     print_listint(head);
     return (0);
 }
