b30db42844d5ce3a36819d83f9e55ac999f743f2
9603nix <mikemachage@gmail.com>
2024-02-15 16:52:50 +0300

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,25 +5,26 @@
  * @a: first number
  * @b: second number
  * @c: third number
- * Return: largest number
+ * Return: 0
  */
 
 int largest_number(int a, int b, int c)
 {
-		int largest;
 
-		if (a >= b && a >= c)
+		if (a > b && a > c)
 		{
-			largest = a;
+			printf("%d is the largest number\n", a);
 		}
-		else if (b >= a && b >= c)
+
+		else if (b > a && b > c)
 		{
-			largest = b;
+			printf("%d is the largest number\n", b);
 		}
+
 		else
 		{
-			largest = c;
+			printf("%d is the largest number\n", c);
 		}
-		return (largest);
+		return (0);
 }
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 #endif
