e4110723373ec12c06aeb1323215da4768542c3d
9603nix <mikemachage@gmail.com>
2024-02-22 00:17:02 +0300

    fn creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
 	/* Check if filename is NULL */
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-	int fd;
 
 	/* Open || creat file with appropriate permissions */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
