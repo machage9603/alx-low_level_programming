c90c636c69c174dbc0e59a273c833ad5b32210e6
machage9603 <mikemachage@gmail.com>
2024-03-11 01:42:54 +0300

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,20 +9,20 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written, len;
+	int fd;
+	ssize_t len, bytes_written;
 
 	if (!filename)
 		return (-1);
-	
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len]; len++);
-	}
+
+	if (text_content == NULL)
+		return (1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
+	len = strlen(text_content);
 	bytes_written = write(fd, text_content, len);
 	if (bytes_written == -1)
 		return (-1);
