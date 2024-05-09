7fb5a5ad51e3875c55f822643a2ee426713dbf59
machage9603 <mikemachage@gmail.com>
2024-05-09 20:01:39 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,20 +11,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i = 0;
-    hash_table_t *table;
+	unsigned long int i = 0;
+	hash_table_t *table;
 
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-        return (NULL);
-    table->array = malloc(sizeof(hash_table_t *) * size);
-    if (table->array == NULL)
-    {
-        free(table);
-        return (NULL);
-    }
-    table->size = size;
-    for (i = 0; i < table->size; i++)
-        table->array[i] = NULL;
-    return (table);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->array = malloc(sizeof(hash_table_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+	return (table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,8 +6,7 @@
 #include <string.h>
 
 /**
- * struct hash_node_s - Node of a hash table
- *
+ * struct hash_node_s - node of a hash table
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -16,14 +15,13 @@
 
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
- *
  * @size: The size of the array
  * @array: An array of the size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -31,8 +29,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
